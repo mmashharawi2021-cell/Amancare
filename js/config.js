e38da0e7a1832b54c346f9752d5ede69ab11423e
mmashharawi2021-cell <mmashharawi2021@gmail.com>
2026-05-02 00:56:35 +0300

    Add storefront config

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const AMANCARE_CONFIG = {
+  storage: {
+    products: 'amancare_products',
+    cart: 'amancare_cart',
+    theme: 'amancare_theme'
+  },
+  adminPassword: 'admin12345',
+  whatsappPhone: '970000000000',
+  currency: '₪',
+  defaultTheme: 'blue'
+};
